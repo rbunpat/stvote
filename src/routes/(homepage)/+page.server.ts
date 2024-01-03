e4fa4e68f41d2899ad5da5404715988a305bfddf
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2024-01-03 08:38:37 +0700

    remove realtime backend thingy

--- src/routes/(homepage)/+page.server.ts
+++ src/routes/(homepage)/+page.server.ts
@@ -1,17 +1,17 @@
-import { db } from '$lib/server/database'
-export const load = async (req: Request, res: Response) => {
+// import { db } from '$lib/server/database'
+// export const load = async (req: Request, res: Response) => {
 
-	try {
-		const contestants = await db.contestant.findMany({
-			select: {
-				name: true,
-				image: true,
-				votes: true
-			}
-		});
-		return {contestants};
-	} catch (error) {
+// 	try {
+// 		const contestants = await db.contestant.findMany({
+// 			select: {
+// 				name: true,
+// 				image: true,
+// 				votes: true
+// 			}
+// 		});
+// 		return {contestants};
+// 	} catch (error) {
 		
-	}
+// 	}
 
-}
+// }
